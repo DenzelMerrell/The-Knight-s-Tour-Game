83a2153425d320082a9b5853ecdda135cdd80f58
DenzelMerrell <denzelmerrell@gmail.com>
2020-10-11 22:56:40 -0400

    The Knights Tour Game

--- src/the/knights/tour/Knight.java
+++ src/the/knights/tour/Knight.java
@@ -13,8 +13,8 @@ The knight begins on the top left square of a chessboard and makes as many
 successive moves as possible with the objective being to visit each square 
 exactly once. For a move to be legal, the destination square must be within the 
 bounds of the chessboard, must not have been visited previously, and must abide 
-by a the movement restrictions of a knight in chess. 
-A tour ends when there are no mor legal moves
+by the movement restrictions of a knight in chess. 
+A tour ends when there are no more legal moves
 
 This program was completed as a part of an assignment for my programming 2 
 class at Florida International University
